8bf25028ba4d1d4fcee33926e55702ce65b0cdea
Folyd <lyshuhow@gmail.com>
2022-12-02 21:40:58 +0800

    Auto create monthly issue

---
+++ new-issue.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Create a new monthly issue
+
+set -e
+
+number=$(($(echo $(ls -d content | wc -l)) + 1))
+month=$(date +'%Y-%m')
+
+mkdir -p content/${month}
+echo "
+number = ${number}
+title = \"${month}\"
+" > content/${month}/zine.toml
